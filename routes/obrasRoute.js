b658ca5172133061079e196e4e9520f9703813dc
Adriel Abner Aparecido <adriel5510@hotmail.com>
2024-05-02 13:53:10 -0300

    Correção cadastro obras

--- routes/obrasRoute.js
+++ routes/obrasRoute.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const ObrasController = require("../controllers/ObrasController");
 const checkToken = require("../helpers/check-tokken");
 
-router.post("/castroObra", checkToken, ObrasController.cadastoobras);
+router.post("/cadastroObra", checkToken, ObrasController.cadastoobras);
 router.get("/verObras", checkToken, ObrasController.verobras);
 router.get("/obra/:id", checkToken, ObrasController.verobra);
 router.put("/atualizaObra/:id", checkToken, ObrasController.atualizaobra);
